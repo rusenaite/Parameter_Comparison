8a04b333ddf90c28ca907a659041e1e0c6317f3f
Austėja Rušėnaitė <austeja.rusenaite@gmail.com>
2021-09-15 16:33:23 +0300

    + ConfigParser Nuget package + FileReader.cs extracts zipped files + ConfigData.cs

---
+++ src/FileReader.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Compression;
+using Salaros.Configuration;
+using System.IO;
+
+namespace ParameterComparison
+{
+    public static class FileReader
+    {
+        /// <summary>
+        /// Method extracts zipped files to a new predefined folder.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns> An extracted files path, if unable to extract - an empty string. </returns>
+        public static string ExtractZippedFiles(string path)
+        {
+            string dataSamplesPath = Path.GetFullPath(Path.Combine(path, @"..\"));
+            string extractPath = Path.Combine(dataSamplesPath, @".\extract");
+            ZipFile.ExtractToDirectory(path, extractPath);
+
+            return extractPath;
+        }
+
+        public static List<dynamic> ReadCfgFile(string path)
+        {
+            List<dynamic> data = new();
+
+            var cfgFile = new ConfigParser(path);
+
+            cfgFile.GetValue("Numbers", "ConfigurationVersion");
+            cfgFile.GetValue("Strings", "FmType");
+
+            return data;
+        }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ParameterComparison
+{
+    class Program
+    {
+        static void Main()
+        {
+            string path = "C:/Users/raust/source/repos/ParameterComparison/data_samples/data.zip";
+
+            string extractedDataPath = FileReader.ExtractZippedFiles(path);
+
+            string sourceFilePath = Path.Combine(extractedDataPath, @".\extract\FMB920-default.cfg");
+            string targetFilePath = Path.Combine(extractedDataPath, @".\extract\FMB920-modified.cfg");
+
+            List <dynamic> sourceCfgFile = FileReader.ReadCfgFile(sourceFilePath);
+            List <dynamic> targetCfgFile = FileReader.ReadCfgFile(targetFilePath);
+
+        }
+    }
+}
